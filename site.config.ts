ebcf10800d0f0416ec301bd7e0e9d0d18b94b25e
1161619559 <36982387+1161619559@users.noreply.github.com>
2022-05-16 15:17:47 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b428c432fb6a4a0db9ca2fea6b2bffe0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '联盟小站 | ZHDN.CN | Beta',
+  domain: 'https://iyou.notion.site/ZHDN-CN-Beta-b428c432fb6a4a0db9ca2fea6b2bffe0',
+  author: 'ZHDN.CN',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: ' ',
+  github: '1258062688',
+  linkedin: ' ',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
